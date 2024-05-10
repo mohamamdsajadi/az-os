4283ffed86b162034a56e217c8c0138aa0501fc9
mohammadsajadi <mohammadsajadi50@gmail.com>
2024-05-10 23:26:46 +0330

    chat network as file transfering

---
+++ ChatNetwork/Client/Client/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class TcpClientSample
+{
+    private const int BufferSize = 1024;
+
+    public static void Main()
+    {
+        int port;
+        string filePath;
+
+        Console.WriteLine("Please enter the port number of the server:");
+        port = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter the path of the file to send:");
+        filePath = Console.ReadLine();
+
+        try
+        {
+            using (TcpClient client = new TcpClient("127.0.0.1", port))
+            {
+                Console.WriteLine("Connected to the server...");
+
+                using (NetworkStream networkStream = client.GetStream())
+                using (FileStream fileStream = File.OpenRead(filePath))
+                {
+                    byte[] buffer = new byte[BufferSize];
+                    int bytesRead;
+
+                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        networkStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    Console.WriteLine("File sent successfully.");
+                }
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Unable to connect to the server.");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
+public class StateObject
+{
+    // Client  socket.
+    public Socket workSocket = null;
+    // Size of receive buffer.
+    public const int BufferSize = 1024;
+    // Receive buffer.
+    public byte[] buffer = new byte[BufferSize];
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+}
---
+++ ChatNetwork/Server/ConsoleApp5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+ 
+namespace ConsoleApp5
+
+
+public class Program
+{
+    private const int BufferSize = 1024;
+
+    public static void Main()
+    {
+        int port;
+
+        Console.WriteLine("Please enter the port number to listen on:");
+        port = Int32.Parse(Console.ReadLine());
+
+        try
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener tcpServer = new TcpListener(localAddr, port);
+            tcpServer.Start();
+
+            Console.WriteLine("Server started. Listening for incoming connections...");
+
+            while (true)
+            {
+                TcpClient client = tcpServer.AcceptTcpClient();
+                Console.WriteLine("Client connected.");
+
+                using (NetworkStream networkStream = client.GetStream())
+                using (FileStream fileStream = File.Create("received_file.dat"))
+                {
+                    byte[] buffer = new byte[BufferSize];
+                    int bytesRead;
+
+                    while ((bytesRead = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fileStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    Console.WriteLine("File received and saved as received_file.dat.");
+                }
+
+                client.Close();
+                Console.WriteLine("Client disconnected.");
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("An error occurred while listening for connections.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}   public class StateObject
+    {
+        // Client  socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 1024;
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+
